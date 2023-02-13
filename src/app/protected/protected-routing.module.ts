6cd96c8cfdd66b1bf255bb3a4a663910d60cec64
Jonathan San Miguel <guerreroz80@outlook.es>
2023-02-13 13:57:52 -0600

    rutas del del protected module listas

--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { GaleryComponent } from './pages/galery/galery.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'galery',
+        component: GaleryComponent
+      },
+      {
+        path: 'form',
+        component: FormularioComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'form'
+      }
+    ]
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
